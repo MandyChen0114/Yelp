d85b768c11ee862c0b22fc779c79c9f6d401fe4b
Mandy Chen <jiayaochen93@gmail.com>
2017-09-23 16:21:52 -0700

    implement category filter

---
+++ Yelp/CategorySwitchCell.swift
@@ -0,0 +1,40 @@
+//
+//  CategorySwitchCell.swift
+//  Yelp
+//
+//  Created by Mandy Chen on 9/23/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol CategorySwitchCellDelegate {
+  @objc optional func categorySwitchCell(categorySwitchCell: CategorySwitchCell, didChangeValue: Bool)
+}
+
+
+class CategorySwitchCell: UITableViewCell {
+
+  @IBOutlet weak var onSwitch: UISwitch!
+  @IBOutlet weak var switchLabel: UILabel!
+  
+  weak var delegate:CategorySwitchCellDelegate?
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    onSwitch.addTarget(self, action: #selector(CategorySwitchCell.switchValueChanged), for: UIControlEvents.valueChanged)
+    
+  }
+  
+  func switchValueChanged() {
+    delegate?.categorySwitchCell?(categorySwitchCell: self, didChangeValue: onSwitch.isOn)
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+
+    // Configure the view for the selected state
+  }
+
+}
